5beb0358a65991e5db8a5a1d19d3597cbede887e
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-06-11 21:04:59 +0330

    fix: remove conversion of mp3 into wav

--- voice_engine/text_to_speech.py
+++ voice_engine/text_to_speech.py
@@ -2,7 +2,7 @@ import os
 from gtts import gTTS
 from uuid import uuid4
 from config import MEDIA_DIR
-from voice_engine.mp3_to_other_type import convert as wav_convertor
+# from voice_engine.mp3_to_other_type import convert as wav_convertor
 
 
 def convert(text: str, language: str = 'en') -> str:
@@ -12,8 +12,9 @@ def convert(text: str, language: str = 'en') -> str:
     exact_file_name = f"{str(uuid4())}.mp3"
     exact_file_path = os.path.join(MEDIA_DIR, exact_file_name)
     tts.save(savefile=exact_file_path)
-    file_path = wav_convertor(audio_path=exact_file_path)
+    # file_path = wav_convertor(audio_path=exact_file_path)
+    # file_path = exact_file_path
 
-    os.remove(exact_file_path)
+    # os.remove(exact_file_path)
 
-    return exact_file_name.replace(".mp3", ".wav"), file_path
+    return exact_file_name, exact_file_path
