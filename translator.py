08ef4460a3174d0d7130ee7dbac81e140f584651
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2023-06-14 20:21:42 +0330

    refactor: remove unused files

--- translator.py
+++
@@ -1,8 +0,0 @@
-from googletrans import Translator
-
-
-def translate(text: str, language: str) -> str:
-    translator = Translator()
-    translated_response = translator.translate(text=text,
-                                               dest=language)
-    return translated_response.text
